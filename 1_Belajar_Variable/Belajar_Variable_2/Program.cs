ad3db60e9d9073ed2689f7192de095bf87d63778
Fauzan2617 <mfauzandwiputera10@gmail.com>
2025-07-10 12:16:40 +0700

    Update Belajar

--- 1_Belajar_Variable/Belajar_Variable_2/Program.cs
+++ 1_Belajar_Variable/Belajar_Variable_2/Program.cs
@@ -2,12 +2,25 @@
 
 // deklarasi variabel Stirng
 string belajar_variable;
+bool belajar_variable_bool;
+int belajar_variable_int;
+float belajar_variable_float;
+char belajar_variable_char;
 
 // inisialisasi atau mengisi variabel String
 string nama = "fauzan";
+bool nama_bool = false;
+int nama_int = 10;
+float nama_float = 3.14f;
+char nama_char = '&';
 
 // menggunanakan/access variabel String
 Console.WriteLine(nama);
+Console.WriteLine(nama_bool);
+Console.WriteLine(nama_int);
+Console.WriteLine(nama_float);
+Console.WriteLine(nama_char);
+
 
 // mengubah isi variabel String
 nama = "fauzan alfarizi";
---
+++ 4_comment&_aturanvariable/4_comment&_aturanvariable/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Komentar");
+// jenis comment
+// ini adalah komentar satu baris
+
+/*
+ * ini adalah komentar
+ * beberapa baris
+ */
+
+/// ini adalah komentar XML
+
+
+
+
+
+Console.WriteLine("Aturan Penamaan Variabel");
+
+// 1. Gunakan camelCase untuk variabel biasa
+// camelCase adalah gaya penamaan di mana kata pertama dimulai dengan huruf kecil dan setiap kata berikutnya dimulai dengan huruf besar.
+string namaDepan = "John";
+
+// 2. Gunakan penggunaan snake_case
+// Gunakan snake_case untuk nama variabel yang berisi beberapa kata
+int umur_depan = 30;
+
+// 3. Gunakan PascalCase untuk nama kelas
+// PascalCase adalah gaya penamaan di mana setiap kata dimulai dengan huruf besar.
+public class NamaKelas{};
+
+
+
+
+
+
+
+
